368d0cbc0dd1b31166174ec4f4317ca107aa517d
Ivan <dima.zagorujko@mail.ru>
2023-04-10 20:52:04 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* alert('Hello'); */
+
+/* const result = confirm('Are you have 18 years old?');
+console.log(result); */
+
+/* const answer = prompt('Вам есть 18 лет?', '18');
+console.log(answer); */
+ 
+/* const answers = [];
+
+answers[0] = prompt('Как вас зовут?','');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Как ваше отчество?', '');
+ 
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+// const category = 'toys';
+
+// console.log(`https://pornhub.com/${category}/5/${category} 2`, 5 + 5);
+
+
+// let incr = 10,
+// 	decr = 10;
+
+//++incr;
+//--decr;
+
+// console.log(--incr, ++decr);
+// console.log(5%2);
+console.log(2 + 5 !==  '7');
+
+const isChecked = false,
+	isClose = false;
+
+console.log(isChecked || !isClose);
+
