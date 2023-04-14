d53874de162c9d5f06f0e81a2fa38898537a3156
Ivan <dima.zagorujko@mail.ru>
2023-04-14 18:19:21 +0300

    Js commit - new web

--- js/index.js
+++ js/index.js
@@ -30,10 +30,38 @@ console.log(typeof(null)); */
 
 // console.log(--incr, ++decr);
 // console.log(5%2);
-console.log(2 + 5 !==  '7');
+// console.log(2 + 5 !==  '7');
 
-const isChecked = false,
-	isClose = false;
+// const isChecked = false,
+// 	isClose = false;
 
-console.log(isChecked || !isClose);
+// console.log(isChecked || !isClose);
+
+
+const numberOfFilms = prompt('Cколько фильмов вы уже посмотрели?', '');
+
+ const personalMovieDB = {
+     count : numberOfFilms,   movies: {},   actors: {},   genres : [],
+     privat: false
+
+ };
+const  a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На сколько оцените его?', '')
+
+
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+ 
+
+
+
+
+
 
